38535fdba383d2a4089ad5eb099b1b82352b72c8
Parker Smith <parker@parkersmith.us>
2017-06-19 18:13:29 -0600

    Add validation

--- dvd_titles.php
+++ dvd_titles.php
@@ -4,4 +4,52 @@ class DvdTitle {
     protected $asin;
     protected $title;
     protected $price;
+
+    public function asin()
+    {
+        return $this->asin;
+    }
+
+    public function title()
+    {
+        return $this->title;
+    }
+
+    public function price()
+    {
+        return $this->price;
+    }
+
+    public function SetAsin($asin)
+    {
+        if (!is_string($asin))
+        {
+            return false;
+        }
+
+        $this->asin = $asin;
+        return true;
+    }
+
+    public function SetTitle($title)
+    {
+        if (!is_string($title))
+        {
+            return false;
+        }
+
+        $this->title = $title;
+        return true;
+    }
+
+    public function SetPrice($price)
+    {
+        if (!is_float($price))
+        {
+            return false;
+        }
+
+        $this->price = $price;
+        return true;
+    }
 }
