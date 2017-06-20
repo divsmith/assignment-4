7dc6e632f5e75e086f22dee68d4e3b2f0cbcbb0d
Parker Smith <parker@parkersmith.us>
2017-06-19 19:02:03 -0600

    Add dvd to ttest

--- mem_test.php
+++ mem_test.php
@@ -1,15 +1,17 @@
 <?php
 
 include "mem.php";
+include "dvd_titles.php";
 
 function TestConstructor()
 {
     // arrange and act
-    $mem = new Mem(7);
+    $dvd = new DvdTitle();
+    $mem = new Mem($dvd);
 
     // assert
     print_r($mem);
-    if ($mem->count() != 1) 
+    if ($mem->count() != 1)
     {
         die('TestConstructor FAILED!' . PHP_EOL);
     }
