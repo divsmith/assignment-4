be81801c6690bbcb1dd627ed5c8e448c52673b5d
Parker Smith <parker@parkersmith.us>
2017-06-21 14:12:41 -0600

    Clean up index.html

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,23 +1,6 @@
 <?php
 
-//include "../dbConnect.php";
 include "../mySQL.php";
-
-//$name = "Kilroy";
-//
-//$db = fConnectToDatabase();
-//
-//$stmt = $db->prepare("INSERT INTO test (name) VALUES (:name)");
-//$stmt->bindParam(":name", $name);
-//$stmt->execute();
-//$id = $db->lastInsertId();
-//print_r($id);
-
-//$statement = $db->prepare("SELECT * FROM test");
-//$statement->execute();
-//while ($row = $statement->fetch()) {
-//    print_r($row);
-//}
 
 fInsertToDatabase('B002ZG980U', 'Inception', 2.22);
 
@@ -29,12 +12,3 @@ foreach ($results as $result)
 }
 
 fDeleteFromDatabase('B002ZG980U');
-
-//echo "<!DOCTYPE html>
-//<html>
-//<head>
-//    <title>test</title>
-//</head>
-//<body>
-//<p>Not implemented</p>
-//</body>";
