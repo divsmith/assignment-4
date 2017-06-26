a57e403b5666479fd6cf59a42de31e274bfb2a90
Parker Smith <parker@parkersmith.us>
2017-06-26 07:48:52 -0600

    Remove unused code

--- htdocs/index.php
+++ htdocs/index.php
@@ -13,24 +13,6 @@ fInsertDVDToDatabase($db, 'B013TYXUXC', 'Interstellar', 12.42);
 fInsertDVDToDatabase($db, 'B000P0J0AQ', 'The Matrix', 7.97);
 fInsertDVDToDatabase($db, 'B00H7KJRVY', 'The Secret Life of Walter Mitty', 5.00);
 
-
-// Show DVD results from database
-//$results = fListDVDsFromDatabase($db);
-//
-//foreach ($results as $result)
-//{
-//    var_dump($result);
-//    echo "<img src=\"http://images.amazon.com/images/P/{$result['asin']}.01.MZZZZZZZ.jpg\"/>";
-//}
-
-// Delete DVDs from database
-//fDeleteDVDFromDatabase($db, 'B002ZG980U');
-//fDeleteDVDFromDatabase($db, 'B000AAF1U4');
-//fDeleteDVDFromDatabase($db, 'B003UESJH4');
-//fDeleteDVDFromDatabase($db, 'B013TYXUXC');
-//fDeleteDVDFromDatabase($db, 'B000P0J0AQ');
-//fDeleteDVDFromDatabase($db, 'B00H7KJRVY');
-
 // Insert Actors into Database
 $id[] = fInsertActorIntoDatabase($db, 'Bill', 'Murray');
 $id[] = fInsertActorIntoDatabase($db, 'Keanu', 'Reeves');
@@ -45,14 +27,6 @@ $id[] = fInsertActorIntoDatabase($db, 'Leonardo', 'DiCaprio');
 $id[] = fInsertActorIntoDatabase($db, 'Ellen', 'Page');
 $id[] = fInsertActorIntoDatabase($db, 'Colin', 'Furth');
 $id[] = fInsertActorIntoDatabase($db, 'Geoffrey', 'Rush');
-
-// List results from Actor's table
-//$results = fListActorsFromDatabase($db);
-//
-//foreach ($results as $result)
-//{
-//    var_dump($result);
-//}
 
 // Insert records into pivot table
 fInsertDVDActor($db, 'B002ZG980U', $id[9]);
